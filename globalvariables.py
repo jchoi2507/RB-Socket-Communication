3d797a413cf208dd2a9bbeffc8afad746db8b48d
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-23 16:00:49 +0900

    Update globalvariables.py

--- globalvariables.py
+++ globalvariables.py
@@ -1,3 +1,5 @@
+## Module for initialization and global variables
+
 import socket
 
         ## Initialization ##
@@ -7,6 +9,13 @@ FORMAT = 'utf-8'
 host = ''
 port = 5678
 server = socket.gethostbyname(socket.gethostname())
+message = " "
+basketNum = " "
+action = " "
 
 commandsArr = ["move1", "move2", "move3", "move4", "shake1", "shake2", "shake3", "shake4",
                "return1", "return2", "return3", "return4"]
+
+        ## Socket initialization ##
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
