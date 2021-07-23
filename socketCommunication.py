9ad2b937b14eff8530f8eb4a70d2be2c908b28ed
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-23 16:01:04 +0900

    Update socketCommunication.py

--- socketCommunication.py
+++ socketCommunication.py
@@ -1,46 +1,45 @@
+## Socket communication & establishment module
+
 import socket
-from globalvariables import *
+import globalvariables as g
 import sys
 import time
 import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print(f"Socket successfully created. Server IP: {server}")
+#Creating a socket communication
+def createSocket():
+    print(f"Socket successfully created. Server IP: {g.server}")
+    g.s.bind((g.server, g.port))
 
-s.bind((server, port))
+#Function send an encoded string message to the RB UI (server -> client)
+def sendMessage(conn, message):
+    encodedMessage = message.encode()
+    conn.send(encodedMessage)
 
-def sendMessage(conn, addr, msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    conn.send(send_length)
-    conn.send(message)
-
+#Function that connects server and client
 def handleClient(conn, addr):
     print(f"New connection at: {addr}")
-
     connected = True
+
     while connected:
+        movement = input("Enter movement (move/shake/return): \n")
+        basketNum = input("Enter basket number: \n")
+        g.message = movement + basketNum
 
-        ##For receiving and printing messages:
+        sendMessage(conn, g.message) #Sending the encoded message to client
 
-        #msg = conn.recv(HEADER).decode(FORMAT)
-        #print(msg)
+        print("Command executing: " + movement + " basket " + basketNum)
+        print(50 * "-")
 
-        ##For sending messages to the RB UI:
-
-        msg = input("Enter desired basket movement.")
-        sendMessage(conn, addr, msg)
-
-def start():
-    s.listen()
+#Actually running the program, with a thread that constantly sends string messages to the RB UI
+def establishCommunication():
+    createSocket()
+    g.s.listen()
     print("Server is listening...")
+
     while True:
-        conn, addr = s.accept()
+        conn, addr = g.s.accept()
         thread = threading.Thread(target=handleClient,args=(conn, addr))
         thread.start()
         print(f"Active connections: {threading.activeCount() - 1}")
-
-start()
-
+        print(50 * "-")
